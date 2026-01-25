d9e60b15c4d363d53e95e05d8ffd6e43e811c364
Fimeri <fi73904@ubt-uni.net>
2026-01-25 23:58:13 +0100

    details.js added

---
+++ details.js
@@ -0,0 +1,29 @@
+const params = new URLSearchParams(window.location.search);
+const service = params.get("service");
+
+const data = {
+health:{
+   title; "Healthcare Services",
+    descrption:"Access health centers,appointments,vaccination information and medical assistance."},
+
+education:{
+    title:"Education Services",
+   descrption:"Find information about schools,registrations,digital platforms and student support".}
+,
+
+admin:{
+    title: "Municipal Administration",
+    descrption:
+    "Submit municipal requests,documents,certificates and administrative services."
+},
+
+
+
+
+
+}
+
+
+
+
+
