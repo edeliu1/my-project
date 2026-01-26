9ece912affd1e59877adb6ebd15662815251fc23
Esulejmani181 <es73599@ubt-uni.net>
2026-01-26 21:52:35 +0100

    add reveal js

---
+++ aboutus.js
@@ -0,0 +1,14 @@
+const sections =
+document.querySelectorAll(".permbajtja");
+
+const reveal = () => {
+    const winH = window.innerHeight;
+      
+    sections.forEach((sec) => {
+        const top = sec.getBoundingClientRect().top;
+        if(top< winH - 120) {
+            sec.classList.add("show");
+        }
+    });
+    
+}
