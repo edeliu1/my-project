2b83b9a743c15a1869c3402c068bf4bc239e7ce8
Fimeri <fi73904@ubt-uni.net>
2026-01-27 21:03:19 +0100

    changes to login.php

--- public/login.php
+++ public/login.php
@@ -1,1 +1,21 @@
+<?php
+session_start();
 
+require_DIR_.'/../app/core/Database.php';
+$config = require_DIR_.'/../config/config.php';
+$db = new Database($config);
+
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] ==='POST'){
+$email = trim ( $_POST['email']??"); 
+
+$password = $_POST['password'] ??";
+
+
+
+
+
+
+}
