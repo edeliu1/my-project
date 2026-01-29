2c546e583976fa91269db73e66a891c526b3374f
Esulejmani181 <es73599@ubt-uni.net>
2026-01-29 19:13:46 +0100

    page

--- public/contact.php
+++ public/contact.php
@@ -5,3 +5,9 @@ $user= $_SESSION['user'] ?? null;
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Contact - Smart City Fushe Kosova</title>
+        <link rel="stylesheet" href="css/index.css">
+</head>
+<body>  
