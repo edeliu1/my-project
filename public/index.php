c4395390d6ca855b568ed470e190a715cabefa60
edeliu1 <ed73490@ubt-uni.net>
2026-01-29 18:50:27 +0100

    contact php created

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ $user = $_SESSION['user'] ?? null;
                 <li><a href="index.php" class="active">Home</a></li>                        
                 <li><a href="about.php">About us</a></li>
                 <li><a href="services.php">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="contact.php">Contact</a></li>
                 <li><a href="login.php">Login</a></li>
 
             </ul> 
