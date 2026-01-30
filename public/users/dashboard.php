623fff4954e2c9dd673558605c5226e1fc55bf8b
Fimeri <fi73904@ubt-uni.net>
2026-01-30 02:03:08 +0100

    add code

--- public/users/dashboard.php
+++ public/users/dashboard.php
@@ -7,3 +7,18 @@ if (isset($_SESSION['user'])) {
         header("Location: ../login.php");
         exit;
 }
+
+$user = $_SESSION['user'];
+
+if (($user['role'] ?? '') === 'admin') {
+        header("Location: ../admin/dashboard.php");
+        exit;
+}
+        ?>
+        <DOCTYPE html>
+         <html lang="en">
+            <head>
+   <meta charset="UTF-8">
+     <meta name="viewport"
+      content="width=device-width, initial-scale=1.0">
+     <title>User Dashboard</title>
