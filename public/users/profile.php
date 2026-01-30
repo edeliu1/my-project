7b6c3487361cd34400c3d306b65029a09e7d89ca
Fimeri <fi73904@ubt-uni.net>
2026-01-30 02:36:45 +0100

    added code

--- public/users/profile.php
+++ public/users/profile.php
@@ -13,7 +13,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $error = "Email not valid.";
  } else {
 
- 
+ $chk = $pdo->prepare("SELECT id FROM users WHERE email = ? AND id <> ?");
+$chk->execute([$email, (int)$user['id']]);
+   if ($chk->fetch()) {
+    $error = "This email is already in use by another user.";
+   }else {
+       $upd = $pdo->prepare("UPDATE users SET name = ?, email = ? WHERE id = ?");
+         $upd->execute([$name, $email, (int)$user['id']]);
+
+         
 
 
 
