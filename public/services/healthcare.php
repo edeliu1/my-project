635c3855b8b542fba86c9418a9938cb5523e2c41
Fimeri <fi73904@ubt-uni.net>
2026-02-01 01:01:54 +0100

    healthcare.php add code

---
+++ public/services/healthcare.php
@@ -0,0 +1,12 @@
+<?phpsession_start();
+$user = $_SESSION['user'] ?? null;
+?>
+<DOCTYPE html>
+    <html lang="en">
+   <head>
+  <meta charset="UTF-8">
+ <title>Healthcare Services</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../css/healthcare.css">
+</head>
+<body>
