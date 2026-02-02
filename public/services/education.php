dc8591bfd587ed8a1c34d2402ff05b3a767e87d7
Esulejmani181 <es73599@ubt-uni.net>
2026-02-02 02:14:10 +0100

    update head

--- public/services/education.php
+++ public/services/education.php
@@ -5,3 +5,9 @@ $user = $_SESSION['user'] ?? null;
 <!doctype html>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Education - Smart City</title>
+        <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
